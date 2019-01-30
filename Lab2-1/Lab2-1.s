d81666f6f5a82a4eaf8bb924c0e0d4a5cb3c7de9
Codey Huntting <creativecodey@gmail.com>
2019-01-29 20:01:00 -0800

    Edited file to work with immediate constants that should be legal

--- Lab2-1/Lab2-1.s
+++ Lab2-1/Lab2-1.s
@@ -2,20 +2,20 @@
 
 _start:
     @ Initialize
-    mov r0, #0xffff 
-    mov r1, #0x91ba
-    mov r2, #0x0649
-    mov r3, #0xb01101111
+    mov r0, #0xff 
+    mov r1, #0x91
+    mov r2, #0x064
+    mov r3, #0xb0
     mov r4, #0x6d
-    mov r5, #0x00001111
-    mov r6, #0xf0001111
-    mov r8, #0xf0000008
+    mov r5, #0x000011
+    mov r6, #0xf1
+    mov r8, #0xf8
         
     @ Execute some logic instructions for testing
     and r0, #0x68
     eor r1, #0x92
     orr r2, #0x3a
-    and r3, #0xb00101101
+    and r3, #0xb0
     and r4, #0x4a
     orr r5, #0x61
     lsl r6, #4

08727dbcdfe21f90710c16cb5a33fe2f3c48d284
Codey Huntting <creativecodey@gmail.com>
2019-02-07 17:43:06 -0800

    Got started on RASM1

--- RASM1/RASM1.s
+++ RASM1/RASM1.s
@@ -1,57 +1,112 @@
 .global _start
 .global _end
 
+.equ BUFSIZE, 32
+
 .data
-prompt: .ascii "Enter a whole number: "
-inbuff: .word
+prompt: .asciz "Enter a whole number: "
+space: .asciz "   "
+int1: .skip BUFSIZE
+int2: .skip BUFSIZE
+int3: .skip BUFSIZE
+int4: .skip BUFSIZE
+addr: .skip 16  // Stores the addresses of the integers to be output
 
 @ Register Table
 @ --------------
 @ r0 -> device   -> device of input/output
 @ r1 -> baseAddr -> base address of memory input/output
-@ r2 -> buffLen  -> length input/output
+@ r2 -> buffLen  -> length input/output, calculated for output
 @ r3 -> internal -> used in stdout/stdin
 
-@ r4 -> loopTimes -> number of times the loop runs
-@ r5 -> LCV       -> the number of times the loop has run
-
 .text
+.balign 4
 _start:
-    @ Initialize the loop
-    _loopinit:
-        mov r4, #4  @ R4 is the number of times the loop runs
-        mov r5, #0  @ R5 is the LCV update each loop
-        cmp r4, r5  @ Compare max loops with number of loops run - it's a pre-check loop
+    @ Prompt for first integer
+    ldr r1, =prompt
+    bl putstring
+
+    ldr r1, =int1
+    mov r2, #BUFSIZE
+    bl getstring
+
+    @ Prompt for second integer
+    ldr r1, =prompt
+    bl putstring
+
+    ldr r1, =int2
+    mov r2, #BUFSIZE
+    bl getstring
+
+    @ Prompt for third integer
+    ldr r1, =prompt
+    bl putstring
+
+    ldr r1, =int3
+    mov r2, #BUFSIZE
+    bl getstring
+
+    @ Prompt for fourth integer
+    ldr r1, =prompt
+    bl putstring
+
+    ldr r1, =int4
+    mov r2, #BUFSIZE
+    bl getstring
+
+    // Store the base address of the data at addr
+    ldr r4, =addr
+
+    // Store the address of int1 in addr
+    ldr r1, =int1
+    str r1, [r4]
     
-    @ Check the loop
-    _loopcheck:
-        beq _loopend    @ Branch to the loop's end if the zero flag is set
-    
-    @ Calculations in the loop
-    _loopbody:
-        @ Output the prompt to the standard output
-        ldr r1, =prompt
-        mov r2, #22
-        bl _stdout
+    // Print the address of int1
+    ldr r1, =addr
+    mov r2, 16
+    bl _stdout
 
-        @ Get input from the keyboard
-        ldr r1, =inbuff
-        mov r2, #32
-        bl _stdin
+    // Put empty space for readability
+    ldr r1, =space
+    bl putstring
 
-	ldr r6, [r1]
+    // Store the address of int2 in addr
+    ldr r1, =int2
+    str r1, [r4]    
 
-        bal _loopchange
+    // Print the address of int2
+    ldr r1, =addr
+    mov r2, 16
+    bl _stdout
 
-    @ Update the LCV for the loop
-    _loopchange:
-        add r5, r5, #1
-	cmp r4, r5 
-        bal _loopcheck
+    // Put empty space for readability
+    ldr r1, =space
+    bl putstring
 
-    _loopend:
-	mov r0, #1
-    	mov r7, #1
-    	swi 0
-	.end
+    // Store the address of int2 in addr
+    ldr r1, =int3
+    str r1, [r4]    
 
+    // Print the address of int2
+    ldr r1, =addr
+    mov r2, 16
+    bl _stdout
+
+    // Put empty space for readability
+    ldr r1, =space
+    bl putstring
+
+    // Store the address of int2 in addr
+    ldr r1, =int4
+    str r1, [r4]    
+
+    // Print the address of int2
+    ldr r1, =addr
+    mov r2, 16
+    bl _stdout
+
+    @ Service call to terminate the program
+    mov r0, #0
+    mov r7, #1
+    swi 0
+    .end

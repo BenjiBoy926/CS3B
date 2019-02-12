0d9d54dfc225be3d282e4ee26661e3508c494829
Codey Huntting <creativecodey@gmail.com>
2019-02-11 16:28:14 -0800

    Got barnetts ascint macro, modified RASM1 to work as a loop

--- RASM1/RASM1.s
+++ RASM1/RASM1.s
@@ -1,59 +1,63 @@
 .global _start
 .global _end
 
-.equ BUFSIZE, 32
+.equ BUFSIZE, 11
 
 .data
 prompt: .asciz "Enter a whole number: "
 space: .asciz "   "
-int1: .skip BUFSIZE
-int2: .skip BUFSIZE
-int3: .skip BUFSIZE
-int4: .skip BUFSIZE
 addr: .skip 16  // Stores the addresses of the integers to be output
+
+// Array contains blocks of memory for the ascii inputs from the user
+inputs:
+.skip BUFSIZE
+.skip BUFSIZE
+.skip BUFSIZE
+.skip BUFSIZE
+endinputs:
 
 @ Register Table
 @ --------------
-@ r0 -> device   -> device of input/output
-@ r1 -> baseAddr -> base address of memory input/output
-@ r2 -> buffLen  -> length input/output, calculated for output
-@ r3 -> internal -> used in stdout/stdin
+@ r0 -> device          -> device of input/output
+@ r1 -> baseAddr        -> base address of memory input/output
+@ r2 -> buffLen         -> length input/output, calculated for output
+@ r3 -> internal        -> used in stdout/stdin
+@ r4 -> currendAddr     -> Current address in the block of memory with all the user inputs
+@ r5 -> inputsEndAddr   -> end address of the block of memory with all the user inputs 
 
 .text
 .balign 4
 _start:
-    @ Prompt for first integer
-    ldr r1, =prompt
-    bl putstring
+    _inputloopinit:
+        // Intialize base address and end address of the array of inputs
+        ldr r4, =inputs
+        ldr r5, =endinputs
 
-    ldr r1, =int1
-    mov r2, #BUFSIZE
-    bl getstring
+    _inputloopcheck:
+        // Branch to the end if r4 >= r5
+        cmp r4, r5
+        beq _loopend
+        bpl _loopend
 
-    @ Prompt for second integer
-    ldr r1, =prompt
-    bl putstring
+    _inputloopbody:
+        @ Prompt for first integer
+        ldr r1, =prompt
+        bl putstring
 
-    ldr r1, =int2
-    mov r2, #BUFSIZE
-    bl getstring
+        @ Get string from console and store it 
+        @ in the block currently pointed to by r4
+        ldr r1, r4
+        mov r2, #BUFSIZE
+        bl getstring
 
-    @ Prompt for third integer
-    ldr r1, =prompt
-    bl putstring
-
-    ldr r1, =int3
-    mov r2, #BUFSIZE
-    bl getstring
-
-    @ Prompt for fourth integer
-    ldr r1, =prompt
-    bl putstring
-
-    ldr r1, =int4
-    mov r2, #BUFSIZE
-    bl getstring
-
+    _inputloopchange:
+        @ Add the size of one item in the input array
+        @ r4 so it points to the next input block and
+        @ branch back to the loop check
+        add r4, r4, #BUFSIZE
+        bal _inputloopcheck
+    
+    _inputloopend:
     // Store the base address of the data at addr
     ldr r4, =addr
 

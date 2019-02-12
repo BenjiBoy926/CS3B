121dd1f9cf5d34a4f410447abdba0e08edd2e5fa
Codey Huntting <creativecodey@gmail.com>
2019-02-11 16:54:13 -0800

    Tested and debugged RASM1 so far

--- RASM1/RASM1.s
+++ RASM1/RASM1.s
@@ -36,8 +36,8 @@ _start:
     _inputloopcheck:
         // Branch to the end if r4 >= r5
         cmp r4, r5
-        beq _loopend
-        bpl _loopend
+        beq _inputloopend
+        bpl _inputloopend
 
     _inputloopbody:
         @ Prompt for first integer
@@ -46,69 +46,18 @@ _start:
 
         @ Get string from console and store it 
         @ in the block currently pointed to by r4
-        ldr r1, r4
+        mov r1, r4
         mov r2, #BUFSIZE
         bl getstring
 
     _inputloopchange:
         @ Add the size of one item in the input array
-        @ r4 so it points to the next input block and
+        // r4 so it points to the next input block and
         @ branch back to the loop check
         add r4, r4, #BUFSIZE
         bal _inputloopcheck
     
     _inputloopend:
-    // Store the base address of the data at addr
-    ldr r4, =addr
-
-    // Store the address of int1 in addr
-    ldr r1, =int1
-    str r1, [r4]
-    
-    // Print the address of int1
-    ldr r1, =addr
-    mov r2, 16
-    bl _stdout
-
-    // Put empty space for readability
-    ldr r1, =space
-    bl putstring
-
-    // Store the address of int2 in addr
-    ldr r1, =int2
-    str r1, [r4]    
-
-    // Print the address of int2
-    ldr r1, =addr
-    mov r2, 16
-    bl _stdout
-
-    // Put empty space for readability
-    ldr r1, =space
-    bl putstring
-
-    // Store the address of int2 in addr
-    ldr r1, =int3
-    str r1, [r4]    
-
-    // Print the address of int2
-    ldr r1, =addr
-    mov r2, 16
-    bl _stdout
-
-    // Put empty space for readability
-    ldr r1, =space
-    bl putstring
-
-    // Store the address of int2 in addr
-    ldr r1, =int4
-    str r1, [r4]    
-
-    // Print the address of int2
-    ldr r1, =addr
-    mov r2, 16
-    bl _stdout
-
     @ Service call to terminate the program
     mov r0, #0
     mov r7, #1

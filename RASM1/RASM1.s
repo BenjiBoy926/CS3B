ef6dc0f268b734b21fff6f5b539d9492f8cb3693
Codey Huntting <creativecodey@gmail.com>
2019-02-13 16:31:09 -0800

    Began writing RASM1, transferring to pi for testing

--- RASM1/RASM1.s
+++ RASM1/RASM1.s
@@ -1,45 +1,59 @@
 .global _start
 .global _end
 
-.equ BUFSIZE, 11
+.equ BUFSIZE, 12
 
 .data
-prompt: .asciz "Enter a whole number: "
-space: .asciz "   "
-addr: .skip 16  // Stores the addresses of the integers to be output
+prompt:     .asciz "Enter a whole number: "
+summary:    .asciz "The addresses of the 4 ints: "
+plus:       .asciz " + "
+minus:      .asciz " - "
+equals:     .asciz " = "
+leftparenthesis:   .byte 40
+rightparenthesis:  .byte 41
+endl:       .byte  13
 
 // Array contains blocks of memory for the ascii inputs from the user
-inputs:
+inputstrings:
 .skip BUFSIZE
 .skip BUFSIZE
 .skip BUFSIZE
 .skip BUFSIZE
-endinputs:
+endinputstrings:
+// Array of data words of the inputs converted to ints
+ints:
+.word 0, 0, 0, 0
+endints:
+// Result of the calculation of the program
+answer:     .word 0
+answerstr:  .skip BUFSIZE
 
 @ Register Table
 @ --------------
-@ r0 -> device          -> device of input/output
-@ r1 -> baseAddr        -> base address of memory input/output
-@ r2 -> buffLen         -> length input/output, calculated for output
-@ r3 -> internal        -> used in stdout/stdin
-@ r4 -> currendAddr     -> Current address in the block of memory with all the user inputs
-@ r5 -> inputsEndAddr   -> end address of the block of memory with all the user inputs 
+@ r0 -> device / intConvertedFromAscii / intsCurrentAddr                     
+@ r1 -> baseAddr                              
+@ r2 -> buffLen                                 
+@ r3 -> internal                                
+@ r4 -> inputCurrentAddr / answer     
+@ r5 -> inputsEndAddr / inputCurrentAddr
+@ r6 -> intsCurrentAddr  
 
 .text
 .balign 4
 _start:
-    _inputloopinit:
+    _loopinit:
         // Intialize base address and end address of the array of inputs
-        ldr r4, =inputs
-        ldr r5, =endinputs
+        ldr r4, =inputstrings
+        ldr r5, =endinputstrings
+        ldr r6, =ints
 
-    _inputloopcheck:
-        // Branch to the end if r4 >= r5
+    _loopcheck:
+        // Branch to the end if currentAddr >= endAddr (r4 >= r5)
         cmp r4, r5
-        beq _inputloopend
-        bpl _inputloopend
+        beq _loopend
+        bpl _loopend
 
-    _inputloopbody:
+    _loopbody:
         @ Prompt for first integer
         ldr r1, =prompt
         bl putstring
@@ -50,14 +64,87 @@ _start:
         mov r2, #BUFSIZE
         bl getstring
 
-    _inputloopchange:
-        @ Add the size of one item in the input array
-        // r4 so it points to the next input block and
-        @ branch back to the loop check
+        @ Branch to ascint macro to convert data pointed to by r1 to an int and store the int in r0
+        mov r1, r4
+	    bl ascint32
+        
+        @ Store r0 into the current byte word pointed to by r6
+        str r0, [r6]
+
+    _loopchange:
+        @ Add the size of one item in the input array to
+        @ r4 so it points to the next input block
         add r4, r4, #BUFSIZE
-        bal _inputloopcheck
+        add r6, r6, #4
+        bal _loopcheck
     
-    _inputloopend:
+    _loopend:
+    // Here, r0 will be used as the pointer to the integers instead of r4
+    ldr r0, =ints
+    ldr r4, [r0], #4
+    ldr r5, [r0], #4
+    ldr r6, [r0], #4
+    ldr r7, [r0]
+    // Calculate (r4 + r5) - (r6 + r7)
+    add r4, r4, r5
+    add r5, r6, r7
+    sub r4, r4, r5
+    // Store the answer in the space at the "answer" label
+    ldr r1, =answer
+    str r4, [r1]
+    // Convert contents of r4 to string and store in "answerstr"
+    mov r0, r4
+    ldr r1, =answerstr
+    bl intasc32
+
+    // Use r4 to iterate through the input strings
+    ldr r4, =inputstrings
+    // Put left parenthesis
+    ldr r1, =leftparenthesis
+    bl putch
+    // Put first input string
+    mov r1, r4
+    add r4, r4, #BUFSIZE
+    bl putstring
+    // Put plus symbol
+    ldr r1, =plus
+    bl putstring
+    // Put second input string
+    mov r1, r4
+    add r4, r4, #BUFSIZE
+    bl putstring
+    // Put right parenthesis
+    ld r1, =rightparenthesis
+    bl putch
+    // Put minus sign
+    ld r1, =minus
+    bl putstring
+    // Put left parenthesis
+    ldr r1, =leftparenthesis
+    bl putch
+    // Put third input string
+    mov r1, r4
+    add r4, r4, #BUFSIZE
+    bl putstring
+    // Put plus symbol
+    ldr r1, =plus
+    bl putstring
+    // Put fourth input string
+    mov r1, r4
+    bl putstring
+    // Put right parenthesis
+    ld r1, =rightparenthesis
+    bl putch
+    // Put plus symbol
+    ldr r1, =equals
+    bl putstring
+    // Put answer string into buffer
+    ldr r1, =answerstr
+    bl putstring
+    // End the line
+    ldr r1, =endl
+    bl putch
+
     @ Service call to terminate the program
     mov r0, #0
     mov r7, #1

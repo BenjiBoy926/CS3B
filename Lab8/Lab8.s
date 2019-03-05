80f5338f3b13db5fe555cbc070352594c61e90d3
Codey Huntting <creativecodey@gmail.com>
2019-03-04 17:14:04 -0800

    Update Lab8 with a loop

--- Lab8/Lab8.s
+++ Lab8/Lab8.s
@@ -35,55 +35,66 @@ _start:
     ldr r1, =date
     ldr r2, =program
     bl OutputHeader
-    // Prompt user for input
-    ldr r1, =inPrompt
-    bl putstring
-    // Store user input into a string
-    ldr r1, =strInput
-    mov r2, #INBUFSIZE
-    bl getstring
-	// Try to conver the string to an integer
-	ldr r1, =strInput
-	bl ascint32
-    // Ascint32 sets the carry flag if conversion is impossible
-	bcs _inputinvalid
-    // Check that input has only numbers
-    _inputvalid:
-        // Check to see if integer input is within range of min-max
-        mov r1, r0
-        mov r2, #MIN
-        mov r3, #MAX
-        bl InRange
-        // Check contents of r0 to see if int value is in range
+    _loopbody:
+        // Prompt user for input
+        ldr r1, =inPrompt
+        bl putstring
+        // Store user input into a string
+        ldr r1, =strInput
+        mov r2, #INBUFSIZE
+        bl getstring
+        // Try to convert the string to an integer
+        ldr r1, =strInput
+        bl ascint32
+        // If integer input is negative, exit the loop
         cmp r0, #0
-        bne _inputinrange
-        beq _inputoutrange
-    _inputinvalid:
-        // Output invalid value message
-        ldr r1, =invalidValMsg
-        bl putstring
-        bal _end
-    _inputinrange:
-        // Get the letter grade if input is out of range    
-        bl GetGrade
-        // Store letter grade in r4 for safekeeping
-        ldr r1, =letterGrade
-	str r0, [r1]
-        // Title string for reporting result
-        ldr r1, =resultReport
-        bl putstring
-        // Put grade character currently stored by r0
-        ldr r1, =letterGrade
-        bl putch
-        // Put an endline
+        blt _end:
+        // "Clear" the cpsr with simple move instruction
+        mov s r1, #1
+        // If "carry" is set and r0 is 0, the string could not be converted to an integer
+        cmp cs r0, #0
+        beq _inputinvalid 
+        _inputvalid:
+            // Check to see if integer input is within range of min-max
+            mov r1, r0
+            mov r2, #MIN
+            mov r3, #MAX
+            bl InRange
+            // Check contents of r0 to see if int value is in range
+            cmp r0, #0
+            bne _inputinrange
+            beq _inputoutrange
+        _inputinvalid:
+            // Output invalid value message
+            ldr r1, =invalidValMsg
+            bl putstring
+            bal _end
+        _inputinrange:
+            // Get the letter grade if input is out of range    
+            bl GetGrade
+            // Store letter grade in r4 for safekeeping
+            ldr r1, =letterGrade
+            str r0, [r1]
+            // Title string for reporting result
+            ldr r1, =resultReport
+            bl putstring
+            // Put grade character currently stored by r0
+            ldr r1, =letterGrade
+            bl putch
+            // Put an endline
+            ldr r1, =endl
+            bl putch
+            bal _end
+        _inputoutrange:
+            // Output appropriate error message if input is out of range
+            ldr r1, =invalidRangeMsg
+            bl putstring
+            bal _end
+    _loopend:
+        // Put an endline and loop again
         ldr r1, =endl
         bl putch
-        bal _end
-    _inputoutrange:
-        // Output appropriate error message if input is out of range
-        ldr r1, =invalidRangeMsg
-        bl putstring
-        bal _end
+        bal _loopbody
     _end:
         // Linux syscall to terminate the program
         mov r0, #0

4ff97097408734290b23243983657ff77ec300e6
Codey Huntting <creativecodey@gmail.com>
2019-03-09 13:02:56 -0800

    Tested and debugged GetIntInput

--- RASM2/GetIntInput.s
+++ RASM2/GetIntInput.s
@@ -1,11 +1,11 @@
 .global GetIntInput
 
-// Amount of bytes allocated for input
-.equ INBUFSIZE 512
-
 .data
+.equ INBUFSIZE, 512
 // Return address, stored to preserve the value
 retAddr:	.word 0
+// Prompt for input
+inputPrompt:	.asciz "Enter a number: "
 // String input by user
 strInput:	.skip INBUFSIZE
 // Messages output in the case of invalid inputs
@@ -20,20 +20,23 @@ can be interpreted as an int, and that the integer is not too big
 ----------------
 */
 
+.balign 4
+.text
 GetIntInput:
 	// Store the contents of the link register
 	ldr r1, =retAddr
 	str lr, [r1]
 	_inloop:
+		// Output the input prompt
+		ldr r1, =inputPrompt
+		bl putstring 
 		// Get string input from the user
-		ldr r1, =inBuffer
-		ldr r2, #INBUFSIZE
+		ldr r1, =strInput
+		mov r2, #INBUFSIZE
 		bl getstring
 		// Convert the input to an integer
-		ldr r1, =inBuffer
+		ldr r1, =strInput
 		bl ascint32
-		// "Clear" the cpsr with simple move instruction
-        movs r1, #1
 		// Input is invalid if r0 = 0 and carry flag is set
 		cmpcs r0, #0
 		beq _inputinvalid

684604c1d6dff86718b3bdff92d7cf62046808cd
Codey Huntting <creativecodey@gmail.com>
2019-03-11 16:34:58 -0700

    Put the output subroutine in the main file for debugging

--- RASM2/RASM2.s
+++ RASM2/RASM2.s
@@ -81,7 +81,29 @@ _start:
     adds r0, r8, r9
     ldr r1, =sumMsg
     ldr r2, =addingOverflowMsg
-    bl OutputCalculationResult
+    // Save the arguments before calling other subroutines
+	mov r4, r1
+	mov r5, r2
+	// Check for an overflow error
+    bvs _overflow
+    // Convert r0 to a string
+    ldr r1, =resultStr
+    bl intasc32
+    // Output message
+    mov r1, r4
+    bl putstring
+    // Output the calculation result
+    ldr r1, =resultStr
+    bl putstring
+    // Put an endline
+    ldr r1, =endl
+    bl putch
+    bal _end:
+    _overflow:
+        // Output the appropriate overflow error message
+        mov r1, r5
+        bl putstring
+    _end:
 
     /*
     DIFFERENCE OF TWO NUMBERS AND OUTPUT
@@ -99,73 +121,8 @@ _start:
     ldr r2, =subtractingOverflowMsg
     bl OutputCalculationResult
 
-    @ _loopbody:
-    @     // Prompt user for input
-    @     ldr r1, =inPrompt
-    @     bl putstring
-    @     // Store user input into a string
-    @     ldr r1, =strInput
-    @     mov r2, #INBUFSIZE
-    @     bl getstring
-    @     // Try to convert the string to an integer
-    @     ldr r1, =strInput
-    @     bl ascint32
-    @     // If integer input is negative, exit the loop
-    @     cmp r0, #0
-    @     blt _end
-    @     // "Clear" the cpsr with simple move instruction
-    @     movs r1, #1
-    @     // If "carry" is set and r0 is 0, the string could not be converted to an integer
-    @     cmpcs r0, #0
-    @     beq _inputinvalid 
-    @     _inputvalid:
-    @         // Check to see if integer input is within range of min-max
-    @         mov r1, r0
-    @         mov r2, #MIN
-    @         mov r3, #MAX
-    @         bl InRange
-    @         // Check contents of r0 to see if int value is in range
-    @         cmp r0, #0
-    @         bne _inputinrange
-    @         beq _inputoutrange
-    @     _inputinvalid:
-    @         // Output invalid value message
-    @         ldr r1, =invalidValMsg
-    @         bl putstring
-    @         bal _loopend
-    @     _inputinrange:
-    @         // Get the letter grade if input is out of range    
-    @         bl GetGrade
-    @         // Store letter grade in r4 for safekeeping
-    @         ldr r1, =letterGrade
-    @         str r0, [r1]
-    @         // Title string for reporting result
-    @         ldr r1, =resultReport
-    @         bl putstring
-    @         // Put grade character currently stored by r0
-    @         ldr r1, =letterGrade
-    @         bl putch
-    @         // Put an endline
-    @         ldr r1, =endl
-    @         bl putch
-    @         bal _loopend
-    @     _inputoutrange:
-    @         // Output appropriate error message if input is out of range
-    @         ldr r1, =invalidRangeMsg
-    @         bl putstring
-    @         bal _loopend
-    @ _loopend:
-    @     // Put an endline and loop again
-    @     ldr r1, =endl
-    @     bl putch
-    @     bal _loopbody
-    @ _end:
-	@ // Output goodbye message
-	@ ldr r1, =goodbyeMsg
-	@ bl putstring
-        // Linux syscall to terminate the program
-        mov r0, #0
-        mov r7, #1
-        swi 0
-        .end
-
+    // Linux syscall to terminate the program
+    mov r0, #0
+    mov r7, #1
+    swi 0
+    .end

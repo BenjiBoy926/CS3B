889549b3ac5ce809fe21775f4b7e5cbf95df4497
Codey Huntting <creativecodey@gmail.com>
2019-03-25 12:22:27 -0700

    Remainder is unsigned, got new library file from professor

--- macros/remainder.s
+++ macros/remainder.s
@@ -10,7 +10,7 @@ retAddr:	.word 0
 /*
 r0 remainder(r1 divisor, r2 dividend)
 -------------------------------------
-Return the signed remainder after the integer division r1 / r2
+Return the unsigned remainder after the integer division r1 / r2
 Overflow is set if r2 = 0
 -------------------------------------
 */
@@ -28,7 +28,13 @@ remainder:
 	// Multiply the result of the division by the dividend
 	mul remainder, dividend, remainder
 	// Subtract the divisor from the remainder
-	sub remainder, divisor, remainder  
+	subs remainder, divisor, remainder  
+	// If remainder is negative, force it to be positive
+	blt _if__remneg
+	bal _end
+	_if__remneg:
+		sub remainder, #1
+		mvn remainder, remainder
 	_end:
 		// Restore the return address and branch back to it
 		ldr lr, =retAddr

a19b82df39743a829b80553792d2dfae05e5b19e
Codey Huntting <creativecodey@gmail.com>
2019-03-25 14:52:32 -0700

    slight style change in setv

--- macros/setv.s
+++ macros/setv.s
@@ -9,7 +9,7 @@ Set the overflow flag
 
 setv:
 	// Push registers locally used onto the stack
-	push {r0, r1, r2}
+	push {r0, r1, r2, lr}
 	// Simultaneously set CARRY and OVERFLOW
 	mov r1, #0x80000000
 	mov r2, #0x1
@@ -18,6 +18,4 @@ setv:
 	mov r1, #0x10
 	asrs r1, #1
 	// Pop registers locally used off of the stack
-	pop {r0, r1, r2}
-	bx lr
-	
+	pop {r0, r1, r2, pc}

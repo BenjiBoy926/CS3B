ba9fbc6e1a029b2b9c69007deb73e107d205c599
Codey Huntting <creativecodey@gmail.com>
2019-03-27 12:07:46 -0700

    Debugged and ran lab10

--- Lab10/Lab10.s
+++ Lab10/Lab10.s
@@ -1,12 +1,28 @@
 .global _start
 
+.data
+name:		.asciz "Codey Huntting"
+date:		.asciz "3/27/2019"
+program:	.asciz "Lab10"
+
+.text
+.balign 4
 _start:
+	// Output a header for the program
+	ldr r0, =name
+	ldr r1, =date
+	ldr r2, =program
+	bl OutputHeader
+	// Move the constants into the registers
 	mov r0, #0x80000000
 	mov r1, #0x80000001
 	mov r2, #0x7fffffff
 	mov r3, #0x7ffffffe
-	mov r4, #0x0001e39e
+	mov r4, #61903
+	lsl r4, #1
 	// Terminate the program
-	mov r0, #1
-	mov r7, #0
-	swi 0
+	mov r0, #0
+	mov r7, #1
+	svc 0
+	.end
+

63e2e564ea48ea42d00fd9ec1b0f5d24c50b79e2
Codey Huntting <creativecodey@gmail.com>
2019-04-03 16:57:47 -0700

    Fixed bug in lab14

--- Lab14/Lab14.s
+++ Lab14/Lab14.s
@@ -9,31 +9,36 @@ endl:			.byte 10
 .text
 .balign 4
 _start:
-	mov r4, #0
+	mov r4, #1
+	mov r5, #10
 	_while__r4_not_maxed:
 		// Branch to end if r4 is the max
 		cmp r4, #0x7fffffff
 		beq _end
 		// Convert the number we're computing the factorial for
-		// and output it
 		mov r0, r4
 		ldr r1, =strbuff 
 		bl intasc32
+		// Output the number
+		ldr r1, =strbuff
 		bl putstring
-		// Output factorial equals string
+		// Output factorial equation string
 		ldr r1, =factorialeq
 		bl putstring
 		// Compute factorial of r4
 		mov r1, r4
 		bl factorial
-		// Output result of factorial computation
+		// Convert factorial result
 		ldr r1, =strbuff
 		bl intasc32
+		// Output factorial result
+		ldr r1, =strbuff
 		bl putstring
-		// Put an endling
+		// Put an endline
 		ldr r1, =endl
 		bl putch
 		// Branch back to start of the loop
+		mul r4, r5, r4
 		bal _while__r4_not_maxed
 	_end:
 	ldr r1, =safemsg

715e9236cd3e73cb556a44aa1d123d8087ee4d80
Codey Huntting <creativecodey@gmail.com>
2019-04-03 14:37:53 -0700

    Started lab 14

---
+++ lib/stdlib/factorial.s
@@ -0,0 +1,34 @@
+.global factorial
+
+/*
+r0 factorial(r1 n)
+------------------
+r0 = r1!
+Uses recursion
+------------------
+*/
+
+factorial:
+	// Preserve link register
+	push {lr}
+	// Base case if r1 <= 1
+	cmp r1, #1
+	ble _factorial__base_case
+	// Recursive case if r1 > 1
+	bal _factorial__recursive_case
+	// In the base case, return 1
+	_factorial__base_case:
+		mov r0, #1
+		pop {pc}
+	_factorial__recursive_case:
+		push {r1}
+		// Compute the factorial of r1 - 1
+		sub r1, r1, #1
+		bl factorial
+		// Restore r1 and multiply 
+		// the result of the recusive call
+		pop {r1}
+		mul r0, r0, r1
+		// Pop lr into the pc
+		pop {pc}
+

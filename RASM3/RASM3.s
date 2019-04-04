765217848a129ffb799e71896ef2240a9dc56618
Codey Huntting <creativecodey@gmail.com>
2019-04-03 17:19:53 -0700

    index of string lib routine ready to debug

--- RASM3/RASM3.s
+++ RASM3/RASM3.s
@@ -5,21 +5,30 @@ str1:	.asciz "Hello"
 str2:	.asciz "Hellotherehowareyou"
 str3:	.asciz "we Hello there"
 str4:	.asciz "Hello"
+str5:	.asciz "wat dee frick"
 
 .text
 .balign 4
 _start:
-	ldr r1, =str1
-	ldr r2, =str4
-	bl String_compare
+	ldr r1, =str2
+	ldr r2, =str1
+	bl String_indexOfString
 
 	ldr r1, =str1
 	ldr r2, =str2
-	bl String_compare
+	bl String_indexOfString
+
+	ldr r1, =str3
+	ldr r2, =str1
+	bl String_indexOfString
 
 	ldr r1, =str1
-	ldr r3, =str3
-	bl String_compare
+	ldr r2, =str1
+	bl String_indexOfString
+
+	ldr r1, =str5
+	ldr r2, =str1
+	bl String_indexOfString
 
 	mov r0, #0
 	mov r7, #1
--- lib/strlib/String_indexOfString.s
+++ lib/strlib/String_indexOfString.s
@@ -16,11 +16,37 @@ String_indexOfString:
 	mov r1, r2
 	bl strlen
 	// If the first string is smaller than the second,
+	// return string not found
 	mov r5, r0
 	cmp r4, r5
-	//blt // end
-	// Initialize r3. 
-	// R3 points to the part of the bigger string we are currently
-	// comparing to the smaller string
-	mov r3, r1
-
+	blt ios__if__string_not_found
+	// Restore r1 and r2
+	pop {r1-r2}
+	// Use r3 as the current index
+	mov r3, #0
+	ios__while__byte_not_null:
+		// Load the next byte of the bigger string into r4
+		ldrb r4, [r1]
+		// If this byte is the null terminator, the string was not found
+		cmp r4, #0
+		beq ios__if__string_not_found
+		// Compare string at r1 with string at r2
+		push {r1, r2}
+		bl String_compare
+		pop {r1, r2}
+		// If strings are equal, we found the string
+		cmp r0, #1
+		beq ios__if__string_found
+		// Increment the index and the current string being checked
+		add r3, r3, #1
+		add r1, r1, #1
+		bal ios__while__byte_not_null
+	// If string is found, return r3, the index
+	ios__if__string_found:
+		mov r0, r3
+		bal ios__end
+	// If string not found, return -1
+	ios__if__string_not_found:
+		mvn r0, #0
+	ios__end:
+		pop {r4-r5, pc}

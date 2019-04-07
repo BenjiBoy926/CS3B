9e533a46d99df48673ce7baf17ff254670235879
Codey Huntting <creativecodey@gmail.com>
2019-04-07 13:15:48 -0700

    Wrote a driver to test new string functions in RAMS3

--- RASM3/RASM3.s
+++ RASM3/RASM3.s
@@ -2,33 +2,65 @@
 
 .data
 str1:	.asciz "Hello"
-str2:	.asciz "Hellotherehowareyou"
-str3:	.asciz "we Hello there"
-str4:	.asciz "Hello"
-str5:	.asciz "wat dee frick"
+str2: 	.asciz "I are I"
+endl:	.byte 10
 
 .text
 .balign 4
 _start:
+	// Put the string
+	ldr r1, =str1
+	bl putstring
+	ldr r1, =endl
+	bl putch
+	// Replace 'l' with 'k' in "Hello"
+	ldr r1, =str1
+	mov r2, #'l'
+	mov r3, #'1'
+	bl String_replace
+	// Display the string
+	ldr r1, =str1
+	bl putstring
+	ldr r1, =endl
+	bl putch
+	ldr r1, =endl
+	bl putch
+
+	// Put the string
 	ldr r1, =str2
-	ldr r2, =str1
-	bl String_indexOfString
+	bl putstring
+	ldr r1, =endl
+	bl putch
+	// Replace 'I' with 'y' in "I are I"
+	ldr r1, =str2
+	mov r2, #'I'
+	mov r3, #'y'
+	bl String_replace
+	// Display the string
+	ldr r1, =str2
+	bl putstring
+	ldr r1, =endl
+	bl putch
+	ldr r1, =endl
+	bl putch
 
+	// Put the string
 	ldr r1, =str1
-	ldr r2, =str2
-	bl String_indexOfString
-
-	ldr r1, =str3
-	ldr r2, =str1
-	bl String_indexOfString
-
+	bl putstring
+	ldr r1, =endl
+	bl putch
+	// Replace 'l' with 'k' in "Hello"
 	ldr r1, =str1
-	ldr r2, =str1
-	bl String_indexOfString
-
-	ldr r1, =str5
-	ldr r2, =str1
-	bl String_indexOfString
+	mov r2, #'z'
+	mov r3, #'6'
+	bl String_replace
+	// Display the string
+	ldr r1, =str1
+	bl putstring
+	ldr r1, =endl
+	bl putch
+	ldr r1, =endl
+	bl putch
 
 	mov r0, #0
 	mov r7, #1

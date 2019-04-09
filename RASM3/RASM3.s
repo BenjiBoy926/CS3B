4946bec626f6d6112df55052bed1d21d864a78c2
Codey Huntting <creativecodey@gmail.com>
2019-04-08 17:27:32 -0700

    Modified makefile to link malloc from libc

--- RASM3/RASM3.s
+++ RASM3/RASM3.s
@@ -1,31 +1,11 @@
 .global _start
 
-.data
-str1a:	.asciz "HEllo"
-str1b:	.asciz "HEllo"
-str2a: 	.asciz "12345"
-str2b:	.asciz "12345"
-str3a:	.asciz "Hello nUMBer 123"
-str3b:	.asciz "Hello nUMBer 123"
-endl:	.byte 10
+.extern malloc
+.extern free
 
-.text
-.balign 4
 _start:
-	ldr r1, =str1a
-	bl TestString1
-	ldr r1, =str1b
-	bl TestString2
-	
-	ldr r1, =str2a
-	bl TestString1
-	ldr r1, =str2b
-	bl TestString2
-
-	ldr r1, =str3a
-	bl TestString1
-	ldr r1, =str3b
-	bl TestString2
+	mov r0, #1
+	bl malloc
 
 	mov r0, #0
 	mov r7, #1

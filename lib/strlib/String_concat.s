246eb51647548940d44a4545b13ec7ec9b125ff1
Codey Huntting <creativecodey@gmail.com>
2019-04-11 17:32:00 -0700

    Refactored string concat to be less confusing

--- lib/strlib/String_concat.s
+++ lib/strlib/String_concat.s
@@ -9,46 +9,45 @@ r0 String_concat(r1 str1, r2 str2)
 Return a pointer to a dynamically allocated string 
 with the characters in r1 followed by the characters in r2
 ----------------------------------
+REGISTER TABLE:
+	r4 = str1
+	r5 = str2
+	r6 = str1Len
+	r7 = str2Len
+	r8 = newString
+----------------------------------
 */
 
 String_concat:
 	// Preserve arguments and the link register
-	push {r1, r2, r4, r5, lr}
+	push {r4-r7, lr}
+	mov r4, r1
+	mov r5, r2
 	// Get the length of string r1 and put it into r4
+	mov r1, r4
 	bl strlen
-	pop {r1, r2}
-	mov r4, r0
+	mov r6, r0
 	// Get the length of string in r2
-	mov r1, r2
-	push {r1, r2}
+	mov r1, r5
 	bl strlen
-	pop {r1, r2}
-	mov r5, r0
+	mov r7, r0
 	// Allocate memory the size of both strings,
 	// plus one for the null terminator
-	add r0, r4, r5
+	add r0, r6, r7
 	add r0, r0, #1
-	push {r1, r2}
 	bl malloc
-	pop {r1, r2}
-	// Copy data from r1 into r0
-	mov r2, r0
-	mov r3, r4
-	push {r0-r2}
+	mov r8, r0
+	// Copy first string (r4) into destination (r8)
+	mov r1, r4
+	mov r2, r8
+	mov r3, r6
 	bl memcpy
-	pop {r0-r2}
-	// Prepare arguments for memcpy: r1 = source
-	mov r1, r2
-	// Prepare arguments for memcpy: r2 = destination
-	// Set destination to basePointer + sizeOfFirstString
-	add r2, r0, r4
-	// Prepare arguments for memcpy: r3 = bytes
-	// Copy the number of characters in string two plus one for null terminator
-	add r3, r5, #1
-	// Copy string pointed to by r2 into
-	// part of the string after r1
-	push {r0}
+	// Copy second string (r5) into part of destination 
+	// just beyond the first string (r8 + r6)
+	mov r1, r5
+	add r2, r8, r6
+	add r3, r7, #1
 	bl memcpy
 	// Restore registers, putting preserved link register contents
 	// into the program counter
-	pop {r0, r4, r5, pc}
+	pop {r4-r7, pc}

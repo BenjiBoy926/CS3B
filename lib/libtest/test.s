c1572a1f1512b2b7f3355385bde1237eaea59f70
Codey Huntting <creativecodey@gmail.com>
2019-04-16 19:04:41 -0700

    Created node constructor test

--- lib/libtest/test.s
+++ lib/libtest/test.s
@@ -1,9 +1,17 @@
 .global _start
 
+.data
+str:	.asciz "Hello, world!"
+
 .text
 .balign 4
 _start:
-	bl List
+	ldr r1, =str
+	bl String_length
+
+	mov r1, r0
+	ldr r0, =str
+	bl Node
 
 	mov r0, #0
 	mov r7, #1

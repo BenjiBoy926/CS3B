4cd1b24bd4523ed161cf1e479702fd15cc2e6388
Codey Huntting <creativecodey@gmail.com>
2019-04-17 12:48:36 -0700

    Created contains ignore case and test

--- lib/libtest/test.s
+++ lib/libtest/test.s
@@ -4,7 +4,7 @@
 str1:	.asciz "Hello, world!"
 str2:	.asciz "How are you, world?!"
 str3:	.asciz "I'm good, how are you, Codey?!"
-find:	.asciz "Hello"
+find:	.asciz "heLLo"
 cCR:	.byte 10
 
 .text
@@ -28,31 +28,13 @@ _start:
 
 	mov r0, r4
 	ldr r1, =find
-	ldr r2, =String_contains
+	ldr r2, =String_containsIgnoreCase
 	ldr r3, =putstring_and_endline
 	bl List_foreachcmp
 
 	mov r0, #0
 	mov r7, #1
 	svc 0
-
-// r0 =boolean String_contains(r1 str, r2 otherStr)
-String_contains:
-	push {lr}
-	bl String_indexOfString
-	
-	// Branch 
-	cmp r0, #0
-	bge scontains__if__index_not_negative
-	bal scontains__if__index_negative
-
-	scontains__if__index_not_negative:
-		mov r0, #1
-		bal scontains__end
-	scontains__if__index_negative:
-		mov r0, #0
-	scontains__end:
-		pop {pc}
 
 // (r1 = string)
 putstring_and_endline:

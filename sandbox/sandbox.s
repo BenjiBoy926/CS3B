68f829ae22f9344dd9b3a13b3e6d0f73afc08316
Codey Huntting <creativecodey@gmail.com>
2019-04-17 16:08:50 -0700

    created program for file io test

--- sandbox/sandbox.s
+++ sandbox/sandbox.s
@@ -1,27 +1,27 @@
 .global _start
-.global _end
 
 .data
-.balign
-bvar: 	.byte 0xff
-hvar:	.hword 0xae62
-ivar: 	.word 0xce004a32
-str: 	.asciz "Hi, there!\n"
+filename:	.asciz "input.txt"
+str:		.skip 512 
 
 .text
 .balign 4
 _start:
-    	ldr r1, =bvar
-	ldr r1, [r1]
+    ldr r0, =filename
+	mov r1, #0
+	mov r2, #0
 	
-	ldr r1, =bvar
-	ldrh r1, [r1]
+	mov r7, #5
+	svc 0
 
-	ldr r1, =bvar
-	ldr r1, [r1] 
+	ldr r1, =str
+	mov r2, #13
+	
+	mov r7, #3
+	svc 0
     	
 	mov r0, #0
 	mov r7, #1
 	svc 0
-    	.end
+    .end
 

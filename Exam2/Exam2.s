11ed2c98286534ef3cc8c14a50c362ce525e4059
Codey Huntting <creativecodey@gmail.com>
2019-04-22 17:34:36 -0700

    Added register output to first loop

--- Exam2/Exam2.s
+++ Exam2/Exam2.s
@@ -13,6 +13,9 @@ resultStr:	.skip 13
 mod:	.asciz " % "
 eq:		.asciz " = "
 
+// Used to output the contents of the registers
+integerBuffer:	.skip 13
+
 // String that introduces all registers
 register_output_prompt:	.asciz "Contents of registers after mod routine:\n"
 
@@ -55,11 +58,22 @@ _start:
 		bl GetIntInput
 		mov oper2, r0
 
-		// Get the remainder oper1 % oper2
-		mov r1, oper1
-		mov r2, oper2
-		bl remainder
-		mov mod, r0
+		cmp lcv, #1
+		beq exm3__if__first_loop
+		bal exm3__elif__not_first_loop
+
+		exm3__if__first_loop:
+			mov r0, oper1
+			mov r1, oper2
+			bl mod_and_check
+			mov mod, r0
+			bal exm3__endif__first_loop
+		exm3__elif__not_first_loop:
+			mov r1, oper1
+			mov r2, oper2
+			bl remainder
+			mov mod, r0
+		exm3__endif__first_loop:
 
 		// Check and store whether vs is set/clear
 		bvs exm3__if__vs_set
@@ -134,6 +148,100 @@ out_mod_eq:
 	ldr r1, =strInt2
 	bl putstring
 	ldr r1, =eq
+	bl putstring
+
+	pop {r4-r8, r10-r12, pc}
+
+// r0 =result mod_and_check(r0 oper1, r1 oper2)
+mod_and_check:
+	push {r4-r8, r10-r12, lr}
+
+	mov r4, #4
+	mov r5, #5
+	mov r6, #6
+	mov r7, #7
+	mov r8, #8
+
+	mov r10, #10
+	mov r11, #11
+	mov r12, #12
+
+	mov r2, r1
+	mov r1, r0
+	bl remainder
+	mov r9, r0
+
+	ldr r1, =endl
+	bl putch
+	ldr r1, =register_prefixes
+	bl putstring
+	mov r0, r4
+	bl output_int
+	ldr r1, =endl
+	bl putch
+	add r1, r1, #6
+
+	bl putstring
+	mov r0, r5
+	bl output_int
+	ldr r1, =endl
+	bl putch
+	add r1, r1, #6
+
+	bl putstring
+	mov r0, r6
+	bl output_int
+	ldr r1, =endl
+	bl putch
+	add r1, r1, #6
+
+	bl putstring
+	mov r0, r7
+	bl output_int
+	ldr r1, =endl
+	bl putch
+	add r1, r1, #6
+
+	bl putstring
+	mov r0, r8
+	bl output_int
+	ldr r1, =endl
+	bl putch
+	add r1, r1, #6
+
+	bl putstring
+	mov r0, r10
+	bl output_int
+	ldr r1, =endl
+	bl putch
+	add r1, r1, #7
+
+	bl putstring
+	mov r0, r11
+	bl output_int
+	ldr r1, =endl
+	bl putch
+	add r1, r1, #7
+
+	bl putstring
+	mov r0, r12
+	bl output_int
+	ldr r1, =endl
+	bl putch
+	ldr r1, =endl
+	bl putch
+
+	mov r0, r9
+
+	pop {r4-r8, r10-r12, pc}
+
+// void output_int(r0 int)
+output_int:
+	push {r4-r8, r10-r12, lr}
+
+	ldr r1, =integerBuffer
+	bl intasc32
+	ldr r1, =integerBuffer
 	bl putstring
 
 	pop {r4-r8, r10-r12, pc}

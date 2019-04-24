ec1386c302084349a9677f5ba27504c064a65808
Codey Huntting <creativecodey@gmail.com>
2019-04-24 15:12:24 -0700

    RASM4 ready for basic testing

---
+++ RASM4/RASM4.s
@@ -0,0 +1,59 @@
+/*
+RASM4
+-----
+A very (very) basic text editor in assembly language
+Maintains a linked list of strings.  The user can do
+each of the following:
+
+1) View strings
+2) Add string
+	a) from keyboard
+	b) from file
+3) Delete string (by index)
+4) Replace string at index with string from keyboard
+5) Search string that contain a substring
+6) Save file to another file called output.txt
+7) Quit
+-----
+*/
+
+.global _start
+
+.equ MIN_OPTION, 1
+.equ MAX_OPTION, 7
+
+// Stores a pointer to the string list used to dynamically store all of the strings
+stringList:	.word 0
+// Feedback displayed if user enters invalid option
+inputInvalidPrompt:	.asciz "*** ERROR: please input a number between 1 and 7 ***\n"
+// Endline ascii code
+endl:	.byte 10
+
+_start:
+	// Construct the central list of the program
+	bl List
+
+	// Store a pointer to the new list in the local static variable
+	ldr r4, =stringList
+	str r0, [r4]
+
+	rasm4__while__input_not_7:
+		// Get a valid integer input between the min-max options
+		mov r0, #MIN_OPTION
+		mov r1, #MAX_OPTION
+		bl GetIntInputInRange
+
+		// Check integer against the max option (equal to quit)
+		cmp r0, #MAX_OPTION
+		beq rasm4__endwhile__input_not_7
+	rasm4__endwhile__input_not_7
+
+	// Destroy the list
+	ldr r0, =list
+	ldr r0, [r0]
+	bl d_List
+
+	// Terminate the program
+	mov r0, #0
+	mov r7, #1
+	svc 0
---
+++ lib/stdlib/GetIntInputInRange.s
@@ -0,0 +1,45 @@
+.global GetIntInputInRange
+
+/*
+r0 GetIntInputInRange(r0 min, r1 max)
+-------------------------------------
+Return an integer in r0 gotten from user input
+that is in the range of [min, max]. Additionally
+checks overflow and if the string is a valid int
+-------------------------------------
+*/
+GetIntInputInRange:
+	push {r4-r8, r10-r12, lr}
+
+	// Preserve arguments
+	mov r4, r0
+	mov r5, r1
+
+	rasm4__while__input_out_of_range:
+		bl GetIntInput
+		mov r6, r0
+		
+		// Check to see if the integer input is a valid option
+		mov r1, r6
+		mov r2, r4
+		mov r3, r5
+		bl inrange
+		
+		// If input out of range, loop again
+		cmp r0, #0
+		beq rasm4__if__input_out_of_range
+
+		// If input in range, branch out of input loop
+		bal rasm4__endwhile__input_out_of_range
+
+		// If input is out of range, give user feedback
+		rasm4__if__input_out_of_range:
+			ldr r1, =inputInvalidPrompt
+			bl putstring
+			bal rasm4__while__input_out_of_range
+	rasm4__endwhile__input_out_of_range:
+
+	// Move the int input back into return register
+	mov r0, r6
+
+	pop {r4-r8, r10-r12, pc}

e55b80c056d73fe39b0d62b6da2ce54ee7b7721c
Codey Huntting <creativecodey@gmail.com>
2019-05-01 16:54:28 -0700

    (hopefully) fixed index of string

--- lib/strlib/String2.s
+++ lib/strlib/String2.s
@@ -274,50 +274,61 @@ Find the index of the other string in this string
 
 String_indexOfString:
 	// Preserve registers from calling routine
-	push {r4-r5, lr}
+	push {r4-r8, r10-r12, lr}
+
+	// Preserve arguments
+	mov r4, r1
+	mov r5, r2
+
 	// Get the length of the first string
-	push {r1, r2}
+	mov r1, r4
 	bl strlen
-	mov r4, r0
-	pop {r1, r2}
+	mov r6, r0
+	
 	// Get the length of the other string
-	push {r1, r2}
-	mov r1, r2
+	mov r1, r5
 	bl strlen
-	pop {r1, r2}
+	mov r7, r0
+
 	// If the first string is smaller than the second,
 	// return string not found
-	mov r5, r0
-	cmp r4, r5
+	cmp r6, r7
 	blt ios__if__string_not_found
+
 	// Use r3 as the current index
-	mov r3, #0
+	mov r6, #0
+
 	ios__while__byte_not_null:
-		// Load the next byte of the bigger string into r4
-		ldrb r4, [r1]
+		// Load the next byte of the bigger string into r7
+		ldrb r7, [r4]
+
 		// If this byte is the null terminator, the string was not found
-		cmp r4, #0
+		cmp r7, #0
 		beq ios__if__string_not_found
+
 		// Compare string at r1 with string at r2
-		push {r1, r2}
+		mov r1, r4
+		mov r2, r5
 		bl String_compare
-		pop {r1, r2}
+
 		// If strings are equal, we found the string
 		cmp r0, #1
 		beq ios__if__string_found
+
 		// Increment the index and the current string being checked
-		add r3, r3, #1
-		add r1, r1, #1
+		add r4, r4, #1
+		add r6, r6, #1
 		bal ios__while__byte_not_null
-	// If string is found, return r3, the index
+
+	// If string is found, return r6, the index
 	ios__if__string_found:
-		mov r0, r3
+		mov r0, r6
 		bal ios__end
 	// If string not found, return -1
 	ios__if__string_not_found:
 		mvn r0, #0
 	ios__end:
-		pop {r4-r5, pc}
+		pop {r4-r8, r10-r12, pc}
 
 /*
 r0 String_indexPtr(r1 str, r2 index)
